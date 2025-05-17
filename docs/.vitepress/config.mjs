ea0e621406980ca9e0ce270a6a85bc70fc2795d5
h1dr4x0 <116759699+h1dr4x0@users.noreply.github.com>
2025-05-17 22:41:09 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:"/canvasium-docs/",
   title: "Canvasium",
   description: "Markup-based canvas rendering engine for Node.js",
   themeConfig: {
